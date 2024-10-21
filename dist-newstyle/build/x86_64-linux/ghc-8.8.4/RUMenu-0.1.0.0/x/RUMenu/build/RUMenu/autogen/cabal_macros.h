d98e71da03cdab6003d943db7ce2800d71e9354e
Nicholas Zortea Graczik <zorteanicholas619@gmail.com>
2024-10-21 15:47:51 -0300

    Feat: realizando requisição e retornando json.

--- dist-newstyle/build/x86_64-linux/ghc-8.8.4/RUMenu-0.1.0.0/x/RUMenu/build/RUMenu/autogen/cabal_macros.h
+++ dist-newstyle/build/x86_64-linux/ghc-8.8.4/RUMenu-0.1.0.0/x/RUMenu/build/RUMenu/autogen/cabal_macros.h
@@ -11,6 +11,17 @@
   (major1) == 0 && (major2) == 1 && (minor) <= 0)
 #endif /* MIN_VERSION_RUMenu */
 
+/* package aeson-2.2.3.0 */
+#ifndef VERSION_aeson
+#define VERSION_aeson "2.2.3.0"
+#endif /* VERSION_aeson */
+#ifndef MIN_VERSION_aeson
+#define MIN_VERSION_aeson(major1,major2,minor) (\
+  (major1) <  2 || \
+  (major1) == 2 && (major2) <  2 || \
+  (major1) == 2 && (major2) == 2 && (minor) <= 3)
+#endif /* MIN_VERSION_aeson */
+
 /* package base-4.13.0.0 */
 #ifndef VERSION_base
 #define VERSION_base "4.13.0.0"
@@ -21,6 +32,50 @@
   (major1) == 4 && (major2) <  13 || \
   (major1) == 4 && (major2) == 13 && (minor) <= 0)
 #endif /* MIN_VERSION_base */
+
+/* package bytestring-0.10.10.1 */
+#ifndef VERSION_bytestring
+#define VERSION_bytestring "0.10.10.1"
+#endif /* VERSION_bytestring */
+#ifndef MIN_VERSION_bytestring
+#define MIN_VERSION_bytestring(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  10 || \
+  (major1) == 0 && (major2) == 10 && (minor) <= 10)
+#endif /* MIN_VERSION_bytestring */
+
+/* package http-client-0.7.17 */
+#ifndef VERSION_http_client
+#define VERSION_http_client "0.7.17"
+#endif /* VERSION_http_client */
+#ifndef MIN_VERSION_http_client
+#define MIN_VERSION_http_client(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  7 || \
+  (major1) == 0 && (major2) == 7 && (minor) <= 17)
+#endif /* MIN_VERSION_http_client */
+
+/* package http-client-tls-0.3.6.3 */
+#ifndef VERSION_http_client_tls
+#define VERSION_http_client_tls "0.3.6.3"
+#endif /* VERSION_http_client_tls */
+#ifndef MIN_VERSION_http_client_tls
+#define MIN_VERSION_http_client_tls(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  3 || \
+  (major1) == 0 && (major2) == 3 && (minor) <= 6)
+#endif /* MIN_VERSION_http_client_tls */
+
+/* package http-types-0.12.4 */
+#ifndef VERSION_http_types
+#define VERSION_http_types "0.12.4"
+#endif /* VERSION_http_types */
+#ifndef MIN_VERSION_http_types
+#define MIN_VERSION_http_types(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  12 || \
+  (major1) == 0 && (major2) == 12 && (minor) <= 4)
+#endif /* MIN_VERSION_http_types */
 
 /* package scotty-0.12.1 */
 #ifndef VERSION_scotty
@@ -44,16 +99,16 @@
   (major1) == 1 && (major2) == 2 && (minor) <= 4)
 #endif /* MIN_VERSION_text */
 
-/* package wai-3.2.4 */
-#ifndef VERSION_wai
-#define VERSION_wai "3.2.4"
-#endif /* VERSION_wai */
-#ifndef MIN_VERSION_wai
-#define MIN_VERSION_wai(major1,major2,minor) (\
-  (major1) <  3 || \
-  (major1) == 3 && (major2) <  2 || \
-  (major1) == 3 && (major2) == 2 && (minor) <= 4)
-#endif /* MIN_VERSION_wai */
+/* package time-1.9.3 */
+#ifndef VERSION_time
+#define VERSION_time "1.9.3"
+#endif /* VERSION_time */
+#ifndef MIN_VERSION_time
+#define MIN_VERSION_time(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  9 || \
+  (major1) == 1 && (major2) == 9 && (minor) <= 3)
+#endif /* MIN_VERSION_time */
 
 /* package wai-extra-3.1.15 */
 #ifndef VERSION_wai_extra
--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,54 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import Data.Text.Lazy (Text)
+import Data.Text.Lazy (Text, pack)
+import Network.HTTP.Types.Header (hContentType)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP.Client (defaultManagerSettings, Manager, httpLbs, parseRequest, responseBody)
+import Network.HTTP.Client.TLS (getGlobalManager)
+import Data.Aeson (Value,decode)
+import Data.Time.Clock (getCurrentTime, utctDayTime)
+import Data.Time.Clock.POSIX (getPOSIXTime)
+-- Função principal que primeiro captura os cookies e depois faz outra requisição usando eles
+
+getBody :: String -> IO (Maybe Value)  -- Alterado para retornar um Maybe Value (JSON)
+getBody url = do
+    manager <- getGlobalManager
+    request <- parseRequest url
+    response <- httpLbs request manager
+    let body = responseBody response
+    return $ decode body  -- Tenta decodificar o corpo como JSON
+
+getInicio :: IO String
+getInicio = do
+    currentTime <- round <$> getPOSIXTime  -- Obtém o tempo atual em segundos
+    return (show currentTime)      -- Converte para milissegundos
+
+getFim :: IO String
+getFim = do
+    inicio <- getInicio
+    let inicioMillis = read inicio :: Int  -- Converte o resultado de volta para Int
+    return (show (inicioMillis + 86400000)) -- Adiciona 86400000 milissegundos (1 dia)
+
 
 main :: IO ()
 main = scotty 3000 $ do
-  middleware logStdoutDev
+    middleware logStdoutDev
 
-  -- Define seus roteadores e manipuladores aqui
-  get "/nextDayRU" $ do
-    text "RUZAO"
+    get "/cardapio" $ do
+        inicio <- liftIO getInicio  -- Chama getInicio
+        fim <- liftIO getFim        -- Chama getFim
+        let url = "https://portal.ufsm.br/ru/publico/buscarCardapio.json?inicio=" 
+                    ++ inicio ++ "&fim=" ++ fim 
+                    ++ "&idRestaurante=1&tiposRefeicao%5B%5D=1&tiposRefeicao%5B%5D=2&tiposRefeicao%5B%5D=3"
+        
+         -- Imprime a URL no console
+        liftIO $ putStrLn $ "Requisição para a URL: " ++ url
+
+
+        body <- liftIO $ getBody url
+        case body of
+            Just jsonBody -> json jsonBody       -- Retorna a resposta como JSON
+            Nothing       -> text "Erro ao processar JSON"  -- Caso o parsing falhe
